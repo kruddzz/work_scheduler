e90ef289231946de1664988903856e00f917ca6b
Kyle Rudderforth <kruddzz@gmail.com>
2020-06-19 22:29:29 -0500

    cleaned up code and reworked hour color change

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 $(document).ready( function() {
 // displays Day, Month date in the header
-const m = moment();
-// console.log(m);
-$("#currentDay").text(m.format("dddd, MMMM Do"));
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 function hourColor () {
     
@@ -18,14 +16,14 @@ function hourColor () {
             $(this).addClass("past");
             $(this).removeClass("present");
             $(this).removeClass("future");
-        } else if (realTime < timeTest) {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-        } else {
+        } else if (realTime === timeTest) {
             $(this).removeClass("past");
             $(this).addClass("present");
             $(this).removeClass("future");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
         }
     });
 }
@@ -45,6 +43,39 @@ function saveText () {
     var text9 = JSON.parse(localStorage.getItem("9:00 am"));
     $("#nine").val("");
     $("#nine").val(text9);
+
+    var text10 = JSON.parse(localStorage.getItem("10:00 am"));
+    $("#ten").val("");
+    $("#ten").val(text10);
+
+    var text11 = JSON.parse(localStorage.getItem("11:00 am"));
+    $("#eleven").val("");
+    $("#eleven").val(text11);
+
+    var text12 = JSON.parse(localStorage.getItem("12:00 pm"));
+    $("#twelve").val("");
+    $("#twelve").val(text12);
+
+    var text1 = JSON.parse(localStorage.getItem("1:00 pm"));
+    $("#one").val("");
+    $("#one").val(text1);
+
+    var text2 = JSON.parse(localStorage.getItem("2:00 pm"));
+    $("#two").val("");
+    $("#two").val(text2);
+
+    var text3 = JSON.parse(localStorage.getItem("3:00 pm"));
+    $("#three").val("");
+    $("#three").val(text3);
+
+    var text4 = JSON.parse(localStorage.getItem("4:00 pm"));
+    $("#four").val("");
+    $("#four").val(text4);
+
+    var text5 = JSON.parse(localStorage.getItem("5:00 pm"));
+    $("#five").val("");
+    $("#five").val(text5);
+
 }
     saveText();
     hourColor();
